7ce0ab17e0b8568cca12f8e841ae86060043bcb4
isyazriena <33883961+isyazriena@users.noreply.github.com>
2018-01-16 10:09:57 +0800

    task completed

--- Exercise-Function-2.cpp
+++ Exercise-Function-2.cpp
@@ -1,9 +1,23 @@
 //this is an unfinish program
 //finish up this program
+//intan syazriena bt. mohd. shahidon
+//a17dw2281
 
-int sum(int x,int y)
+#include <iostream>
+using namespace std;
+
+int saje_je;
+
+int sum (int x,int y)
 {
 	int result;
 	result = x+y;
+	return (result);
 
+}
+
+int main ()
+{
+	cout << sum (1,2) << endl;
+	cin >> saje_je;
 }
